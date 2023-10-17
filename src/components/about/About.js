1e56a8b3b2b8ddcf00c911d8d7c8eacfb107569e
nGhart <107068059+nGhart@users.noreply.github.com>
2023-10-17 14:52:53 +0000

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './about.scss';
+import pic from '../../assets/pic.jpg';
 
 const About = () => {
   return (
@@ -9,10 +10,7 @@ const About = () => {
       <div className="aboutSection">
         <div className="aboutImage">
           <div className="shadow">
-            <img
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80"
-              alt=""
-            />
+            <img src={pic} alt="" />
           </div>
           <div className="imageContainer"></div>
         </div>
@@ -24,20 +22,7 @@ const About = () => {
           <p className="font3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            {/* Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. */}
           </p>
-          <div className="credit font2">
-            Photo by{' '}
-            <a href="https://unsplash.com/@michaeldam?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-              Michael Dam
-            </a>{' '}
-            on{' '}
-            <a href="https://unsplash.com/photos/mEZ3PoFGs_k?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-              Unsplash
-            </a>
-          </div>
         </div>
       </div>
     </div>
