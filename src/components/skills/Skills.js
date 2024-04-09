0cc58b59217e6c3a2ff468af602896483f8c5ad7
nGhart <107068059+nGhart@users.noreply.github.com>
2024-04-09 18:37:06 +0000

    Update Skills.js

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -16,91 +16,36 @@ const Skills = () => {
           <div className="skillImage">
             <img src={html} alt="React icon" />
           </div>
-          <div className="skillText">
-            <h6 className="font3">Lorem Ipsum</h6>
-            <p className="font2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
-            </p>
-            <p className="font2">
-              Ut enim ad minim veniam, quis laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </p>
-          </div>
+          
         </div>
         <div className="skillContainer">
           <div className="skillImage">
             <img src={css} alt="React icon" />
           </div>
-          <div className="skillText">
-            <h6 className="font3">Lorem Ipsum</h6>
-            <p className="font2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
-            </p>
-            <p className="font2">
-              Ut enim ad minim veniam, quis laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </p>
-          </div>
+          
         </div>
         <div className="skillContainer">
           <div className="skillImage">
             <img src={react} alt="React icon" />
           </div>
-          <div className="skillText">
-            <h6 className="font3">Lorem Ipsum</h6>
-            <p className="font2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
-            </p>
-            <p className="font2">
-              Ut enim ad minim veniam, quis laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </p>
-          </div>
+          
         </div>
         <div className="skillContainer">
           <div className="skillImage">
             <img src={js} alt="JS icon" />
           </div>
-          <div className="skillText">
-            <h6 className="font3">Lorem Ipsum</h6>
-            <p className="font2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
-            </p>
-            <p className="font2">
-              Ut enim ad minim veniam, quis laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </p>
-          </div>
+          
         </div>
         <div className="skillContainer">
           <div className="skillImage">
             <img src={boot} alt="React icon" />
           </div>
-          <div className="skillText">
-            <h6 className="font3">Lorem Ipsum</h6>
-            <p className="font2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
-            </p>
-            <p className="font2">
-              Ut enim ad minim veniam, quis laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </p>
-          </div>
-        </div>
+                  </div>
         <div className="skillContainer">
           <div className="skillImage">
             <img src={sass} alt="Sass icon" />
           </div>
-          <div className="skillText">
-            <h6 className="font3">Lorem Ipsum</h6>
-            <p className="font2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
-            </p>
-            <p className="font2">
-              Ut enim ad minim veniam, quis laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </p>
-          </div>
+          
         </div>
       </div>
     </div>
